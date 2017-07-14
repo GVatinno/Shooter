151326e185b6de99ffb99ff0a88386182aa0fa06
gianluca.vatinno <gianluca.vatinno@king.com>
2017-07-14 17:25:53 +0100

    adding character

---
+++ Assets/Minion/HumanController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : MonoBehaviour {
+
+	public float minionSpeed = 50.0f;
+
+	private Minion mMinion;
+
+
+	// Use this for initialization
+	void Start () {
+		mMinion = GetComponent<Minion> ();
+		if ( mMinion != null )
+			mMinion.PlayIdle ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("1"))
+			mMinion.PlayIdle ();
+		if (Input.GetKeyDown ("2"))
+			mMinion.PlayDamaged ();
+		
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		float veclocityX = x * Time.deltaTime * minionSpeed;
+		float veclocityZ = y * Time.deltaTime * minionSpeed;
+
+		mMinion.MoveAnimationInDirectionFBLR (new Vector2 (x, y));
+		mMinion.ApplyVelocity(new Vector3(-veclocityX, 0.0f, veclocityZ));
+	}
+}
---
+++ Assets/Minion/Minion.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minion : MonoBehaviour {
+
+	private Animator mAnim;
+	private Rigidbody mRigidBody;
+	private string[] mIdleAnims;
+	private const int mIdleAnimCount = 4;
+	private string[] mDamagedAnims;
+	private const int mDamagedAnimCount = 2;
+
+	// Usawakee this for initialization
+	void Awake () {
+		mAnim = GetComponent<Animator> ();
+		mRigidBody = GetComponent<Rigidbody> ();
+		mIdleAnims = new string[mIdleAnimCount];
+		mIdleAnims[0] = "WAIT01";
+		mIdleAnims[1] = "WAIT02";
+		mIdleAnims[2] = "WAIT03";
+		mIdleAnims[3] = "WAIT04";
+		mDamagedAnims = new string[mDamagedAnimCount];
+		mDamagedAnims[0] = "DAMAGED00";
+		mDamagedAnims[1] = "DAMAGED01";
+	}
+
+	public void PlayIdle()
+	{
+		int index = Random.Range (0, mIdleAnimCount);
+		if (mAnim != null)
+			mAnim.Play (mIdleAnims[index], -1, 0.0f);
+	}
+
+	public void PlayDamaged()
+	{
+		int index = Random.Range (0, mDamagedAnimCount);
+		if (mAnim != null)
+			mAnim.Play (mDamagedAnims[index], -1, 0.0f);
+	}
+
+	public void MoveAnimationInDirectionFBLR(Vector2 direction)
+	{
+		mAnim.SetFloat ("MoveX", direction.x);
+		mAnim.SetFloat ("MoveY", direction.y);
+	}
+
+	public void ApplyVelocity(Vector3 velocity)
+	{
+		mRigidBody.velocity = velocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
